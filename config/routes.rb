10b554ee32f75eb9936248860cdcb343ab3eba5a
hqc <quangchinh11t1@gmail.com>
2020-11-24 01:18:17 +0700

    sign up screen, sign in screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/sign_up", to: "demo_screen#sign_up"
+  get "/sign_in", to: "demo_screen#sign_in"
 end
