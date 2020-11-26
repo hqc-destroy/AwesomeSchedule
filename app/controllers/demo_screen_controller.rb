2aa05c4aed9cdadb7e686f9247cf0b32b4962fe1
Tran Huy Hoang <tran.huy.hoangb@sun-asterisk.com>
2020-11-26 20:51:28 +0700

    delete file in view folder and controller folder

--- app/controllers/demo_screen_controller.rb
+++
@@ -1,9 +0,0 @@
-class DemoScreenController < ApplicationController
-  def sign_up
-    render "./sign_up"
-  end
-
-  def sign_in
-    render "./sign_in"
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users
-  get "/sign_up", to: "demo_screen#sign_up"
-  get "/sign_in", to: "demo_screen#sign_in"
+
 end
