de19870d68cc75aeb93087b0072d2002b08ae6fb
Tran Huy Hoang <tran.huy.hoangb@sun-asterisk.com>
2020-12-06 16:53:32 +0700

    --noedit

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,10 @@ class Task < ApplicationRecord
   TASKS_PARAMS = %i(user_id tasks start_time end_time deadline content level status type_task).freeze
 
   belongs_to :user
+  scope :day, -> (start_date) {where("Date(start_time) = ?", start_date).order("status ASC")}
+  class << self
+    def all_task_one_day day
+      return Task.day day
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   default_url_options :host => "localhost:3000"
-  
   root "tasks#index"
-
+  post '/task/date', to:'oneday#show'
   devise_for :users
   resources :tasks
 end
